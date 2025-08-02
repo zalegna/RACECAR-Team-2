579fe447eceae9473aacec6a321a4dd52d01dfac
zalegna <angelazhao203@gmail.com>
2025-08-02 14:05:59 -0400

    Update grand_prix_final.py

--- Grand_Prix/grand_prix_final.py
+++ Grand_Prix/grand_prix_final.py
@@ -3,9 +3,9 @@ MIT BWSI Autonomous RACECAR
 
 Title: Grand Prix!!!!!!!!
 
-Author: team MONOPOLY (2)
+Author: Team MONOPOLY (2)
 
-Purpose: to win the grand prix lesgoo
+Purpose: Wall follow through the grand prix course. Prints telemetry info to the terminal (lidar dist, current state, etc.)
 """
 
 ########################################################################################
